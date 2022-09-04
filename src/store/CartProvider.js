f41ebfcb33a84fcba07200910ec92a0eee87d815
kononiaka <kononiaka.vladimir@gmail.com>
2022-09-04 17:33:28 +0300

    animations added

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,11 +8,47 @@ const defaultState = {
 
 const cartReducer = (state, action) => {
     if (action.type === "ADD") {
-        const updatedState = state.items.concat(action.item);
         const updatedTotalAmount = state.totalAmount + action.item.amount * action.item.price;
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
+        const existingCartItem = state.items[existingCartItemIndex];
+
+        // let updatedItem;
+        let updatedItems;
+
+        if (existingCartItem) {
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        } else {
+            // updatedItem = { ...action.item };
+            updatedItems = state.items.concat(action.item);
+        }
+
+        // const updatedState = state.items.concat(action.item);
 
         return {
-            items: updatedState,
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+    }
+    if (action.type === 'REMOVE') {
+        const existingItemIndex = state.items.findIndex(item => item.id === action.id);
+        const existingItem = state.items[existingItemIndex];
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
+
+        let updatedItems;
+        if (existingItem.amount === 1) {
+            updatedItems = state.items.filter(item => item.id !== action.id);
+        } else {
+            const updateItem = { ...existingItem, amount: existingItem.amount - 1 };
+            updatedItems = [...state.items];
+            updatedItems[existingItemIndex] = updateItem;
+        }
+        return {
+            items: updatedItems,
             totalAmount: updatedTotalAmount
         };
     }
